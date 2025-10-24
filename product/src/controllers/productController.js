5c1b1b01fcacf7785edebaec0e673113f24b728b
hungei <lqhung1002@gmail.com>
2025-10-24 21:17:36 +0700

    test

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,7 +110,11 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-
+  async getProductsById(req , res, next){
+    const token = req.headers.authorization;
+    const products = await Product.findById(req.params.id);
+    res.status(200).json(products);
+  }
 }
 
 module.exports = ProductController;
